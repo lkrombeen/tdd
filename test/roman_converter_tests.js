d89a2260923e764a4dfd7d2a59c2ce0bc58db6cb
Martijn van den Hoek <martijn.van.den.hoek@navara.nl>
2022-09-27 14:17:35 +0200

    Add ability to convert 5

--- test/roman_converter_tests.js
+++ test/roman_converter_tests.js
@@ -1,6 +1,10 @@
 const assert = require("assert")
 
 function convert(inputNum){
+    if (inputNum === 5){
+        return "V"
+    }
+
     let output = ""
     for (let i = 0; i < inputNum; i++) {
         output += "I"
@@ -13,6 +17,7 @@ describe("Converts integers into roman numbers", () => {
         {input: 1, expected: "I"},
         {input: 2, expected: "II"},
         {input: 3, expected: "III"},
+        {input: 5, expected: "V"},
     ].forEach(({input, expected}) => {
         it(`should convert ${input} into ${expected}`, function () {
             assert.strictEqual(convert(input), expected)
