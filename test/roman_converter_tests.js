c2978d3b14532c97b6c19c358f394eae0ef01b17
Lars Krombeen <larskrombeen@groendus.nl>
2022-09-27 14:56:51 +0200

    Rename vars

--- test/roman_converter_tests.js
+++ test/roman_converter_tests.js
@@ -3,16 +3,16 @@ const assert = require("assert")
 function convert(inputNum) {
     let output = "";
 
-    let romanNumerals = ["M", "CM", "D", "C", "XC", "L", "X", "IX", "V", "IV", "I"];
-    let romansMapped = [1000, 900, 500, 100, 90, 50, 10, 9, 5, 4, 1];
+    let romans = ["M", "CM", "D", "C", "XC", "L", "X", "IX", "V", "IV", "I"];
+    let numerals = [1000, 900, 500, 100, 90, 50, 10, 9, 5, 4, 1];
 
     let index = 0;
     while (inputNum > 0) {
-        const mappedVal = romanNumerals[index];
-        const mappedNum = romansMapped[index];
-        if (inputNum >= mappedNum) {
-            inputNum -= mappedNum;
-            output += mappedVal
+        const roman = romans[index];
+        const numeral = numerals[index];
+        if (inputNum >= numeral) {
+            inputNum -= numeral;
+            output += roman
         } else {
             index += 1
         }
@@ -42,7 +42,7 @@ describe("Converts integers into roman numbers", () => {
         {input: 1000, expected: "M"},
         {input: 3999, expected: "MMMCMXCIX"},
     ].forEach(({input, expected}) => {
-        it(`should convert ${input} into ${expected}`, function () {
+        it(`should convert ${input} into ${expected}`, () => {
             assert.strictEqual(convert(input), expected)
         });
     })
