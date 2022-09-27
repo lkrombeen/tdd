03b22694e919203e0611353290e9ed86d03944ce
Lars Krombeen <larskrombeen@groendus.nl>
2022-09-27 14:47:01 +0200

    Add numbers for up to 3999

--- test/roman_converter_tests.js
+++ test/roman_converter_tests.js
@@ -1,20 +1,19 @@
 const assert = require("assert")
 
-function convert(inputNum){
+function convert(inputNum) {
     let output = "";
 
-    let romanNumerals = ["V", "IV", "I"];
-    let romansMapped = [5, 4, 1];
+    let romanNumerals = ["M", "CM", "D", "C", "XC", "L", "X", "IX", "V", "IV", "I"];
+    let romansMapped = [1000, 900, 500, 100, 90, 50, 10, 9, 5, 4, 1];
 
     let index = 0;
     while (inputNum > 0) {
         const mappedVal = romanNumerals[index];
         const mappedNum = romansMapped[index];
-        if (inputNum >= mappedNum){
+        if (inputNum >= mappedNum) {
             inputNum -= mappedNum;
             output += mappedVal
-        }
-        else {
+        } else {
             index += 1
         }
     }
@@ -30,6 +29,18 @@ describe("Converts integers into roman numbers", () => {
         {input: 4, expected: "IV"},
         {input: 5, expected: "V"},
         {input: 6, expected: "VI"},
+        {input: 7, expected: "VII"},
+        {input: 8, expected: "VIII"},
+        {input: 9, expected: "IX"},
+        {input: 10, expected: "X"},
+        {input: 50, expected: "L"},
+        {input: 89, expected: "LXXXIX"},
+        {input: 90, expected: "XC"},
+        {input: 100, expected: "C"},
+        {input: 500, expected: "D"},
+        {input: 900, expected: "CM"},
+        {input: 1000, expected: "M"},
+        {input: 3999, expected: "MMMCMXCIX"},
     ].forEach(({input, expected}) => {
         it(`should convert ${input} into ${expected}`, function () {
             assert.strictEqual(convert(input), expected)
