75a4be9c69ab29f6fbc51c657d7baed50c8eaac7
Martijn van den Hoek <martijn.van.den.hoek@navara.nl>
2022-09-27 14:18:05 +0200

    Add ability to convert 4

--- test/roman_converter_tests.js
+++ test/roman_converter_tests.js
@@ -4,6 +4,9 @@ function convert(inputNum){
     if (inputNum === 5){
         return "V"
     }
+    if (inputNum === 4){
+        return "IV"
+    }
 
     let output = ""
     for (let i = 0; i < inputNum; i++) {
@@ -17,6 +20,7 @@ describe("Converts integers into roman numbers", () => {
         {input: 1, expected: "I"},
         {input: 2, expected: "II"},
         {input: 3, expected: "III"},
+        {input: 4, expected: "IV"},
         {input: 5, expected: "V"},
     ].forEach(({input, expected}) => {
         it(`should convert ${input} into ${expected}`, function () {
